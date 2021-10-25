88b5f60dd3444bb51fb83cc5f2958c01205db425
antonyuhnovets <Vaker1990@gmail.com>
2021-10-25 03:40:47 +0300

    BOT-03: fix bugs, main task complete

--- webdriver.py
+++ webdriver.py
@@ -1,4 +1,5 @@
 import time
+import os
 import pandas as pd
 
 from RPA.Browser.Selenium import Selenium
@@ -8,40 +9,35 @@ import settings
 
 
 class WebDriver(Selenium):
-
-    browser_lib = Selenium()
+    browser = Selenium()
 
     # Open browser, set download path and get url
-    def open_web_browser(self, url):
-        self.browser_lib.set_download_directory(
+    def open_web_browser(self):
+        self.browser.set_download_directory(
             directory=settings.DOWNLOAD_DIR,
             download_pdf=True
         )
 
-        self.browser_lib.open_available_browser()
+        self.browser.open_available_browser()
 
     # Click the button to open
-    # TODO: wait untill page loaded
     def click(self, locator_value):
-        self.browser_lib.click_element_when_visible(
+        self.browser.click_element_when_visible(
             locator=locator_value,
         )
 
     # Search for agencies and amounts and add to list
-    def search_for_agencies_and_amount(self, container_locator, agencie_locator_value, amount_locator_value):
-        self.browser_lib.wait_until_element_is_visible(
-            locator=container_locator
-        )
+    def search_for_agencies_and_amount(self, agencie_locator_value, amount_locator_value):
 
-        agencies_list = self.browser_lib.find_elements(
+        agencies_list = self.browser.find_elements(
             locator=agencie_locator_value,
         )
 
-        amount_list = self.browser_lib.find_elements(
+        amount_list = self.browser.find_elements(
             locator=amount_locator_value,
         )
 
-        agencies_and_amount_dict = {k.text: v.text for (k,v) in zip(agencies_list, amount_list)}
+        agencies_and_amount_dict = {k.text: v.text for (k, v) in zip(agencies_list, amount_list)}
 
         df = pd.DataFrame(
             data=agencies_and_amount_dict.items(),
@@ -52,27 +48,26 @@ class WebDriver(Selenium):
 
     # Get the agencie url for scraping table
     def get_agencie_url(self, agencie_name):
-        agencie_url = self, self.browser_lib.find_element(
+        agencie_url = self, self.browser.find_element(
             locator=f'xpath://div[contains(@class,"wrapper")]//a[contains(.,"{agencie_name}")]',
         )
 
         return agencie_url
 
     # Set full table view
-    # TODO: wait untill table loads
     def set_table_view(self, value_locator, key):
-        self.browser_lib.press_key(
+        self.browser.press_key(
             locator=value_locator,
             key=key,
         )
 
     # Get the Individual Investments table
     def get_table(self, column_names_locator, rows_locator):
-        column_names = self.browser_lib.find_elements(
+        column_names = self.browser.find_elements(
             locator=column_names_locator,
         )
 
-        rows_parents = self.browser_lib.find_elements(
+        rows_parents = self.browser.find_elements(
             locator=rows_locator,
         )
 
@@ -86,9 +81,8 @@ class WebDriver(Selenium):
         return df
 
     # Get the UII links from table
-    # TODO: if element contain href
     def get_UII_links(self, link_locator):
-        link_elemens_list = self.browser_lib.find_elements(
+        link_elemens_list = self.browser.find_elements(
             locator=link_locator,
         )
 
@@ -97,19 +91,27 @@ class WebDriver(Selenium):
         return link_list
 
     # Download pdf files
-    # TODO: wait until page loads, wait until download complete
-    def download_pdf(self, link_list):
+    def download_pdf(self, link_list, pdf_locator):
         for link in link_list:
-            time.sleep(2)
-
-            self.browser_lib.go_to(
-                url=link,
+            self.browser.go_to(
+                url=link
+            )
+            self.browser.wait_until_element_is_visible(
+                locator=pdf_locator,
             )
 
-            time.sleep(2)
+            try:
+                self.browser.click_element(
+                    locator=f'{pdf_locator}/a',
+                )
+                while self.browser.does_page_contain(
+                    text='Generating PDF...'):
+                    time.sleep(1)
+                else:
+                    continue
+            except:
+                continue
 
-            self.browser_lib.click_element_when_visible(
-                locator='//*[@id="business-case-pdf"]/a',
-            )
-
-            time.sleep(3)
+        while any([filename.endswith(".crdownload") for filename in
+                   os.listdir(settings.DOWNLOAD_DIR)]):
+            time.sleep(1)
